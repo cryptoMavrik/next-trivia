e00ffe8c618d2d5af33f85c39ad067bbae4a026e
cryptoMavrik <xdaegonx@gmail.com>
2022-08-19 09:55:30 -0400

    add questions

--- src/config/questions.ts
+++ src/config/questions.ts
@@ -50,6 +50,61 @@ export const questions: QuestionType[] = [
             3: "Linus Torvalds",
             4: "Vitalik Buterin"
         },
+        correctAnswer: 4
+    },
+    {
+        id: 5,
+        phrase: "Who created Bitcoin?",
+        choices: {
+            1: "Satoshi Nakamoto",
+            2: "Craig Wright",
+            3: "Linus Torvalds",
+            4: "Vitalik Buterin"
+        },
         correctAnswer: 1
-    }
+    },
+    {
+        id: 6,
+        phrase: "Who created Bitcoin?",
+        choices: {
+            1: "Satoshi Nakamoto",
+            2: "Craig Wright",
+            3: "Linus Torvalds",
+            4: "Vitalik Buterin"
+        },
+        correctAnswer: 2
+    },
+    {
+        id: 7,
+        phrase: "Who created Bitcoin?",
+        choices: {
+            1: "Satoshi Nakamoto",
+            2: "Craig Wright",
+            3: "Linus Torvalds",
+            4: "Vitalik Buterin"
+        },
+        correctAnswer: 2
+    },
+    {
+        id: 8,
+        phrase: "Who created Bitcoin?",
+        choices: {
+            1: "Satoshi Nakamoto",
+            2: "Craig Wright",
+            3: "Linus Torvalds",
+            4: "Vitalik Buterin"
+        },
+        correctAnswer: 4
+    },
+    {
+        id: 9,
+        phrase: "Who created Bitcoin?",
+        choices: {
+            1: "Satoshi Nakamoto",
+            2: "Craig Wright",
+            3: "Linus Torvalds",
+            4: "Vitalik Buterin"
+        },
+        correctAnswer: 1
+    },
 ]
