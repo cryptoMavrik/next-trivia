3babe006af1925163604bfaab8c293ed3050700e
cryptoMavrik <xdaegonx@gmail.com>
2022-08-18 22:11:33 -0400

    add chain info

---
+++ src/config/chainInfo.ts
@@ -0,0 +1,4 @@
+export const BASE_EXPLORER_URL = "https://etherscan.io"
+export const COIN_NAME = "ETH"
+export const CHAIN_NAME = "Ethereum"
+export const RPC_URL = `https://mainnet.infura.io/v3/81cf2554caff4f3da20cf2cf797c578d`
