8a11bd08ac96d1280526895ca970c74dfa1d4743
cryptoMavrik <xdaegonx@gmail.com>
2022-08-18 23:11:15 -0400

    add web3 provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
+import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
 import type { AppProps } from 'next/app'
 
+const getLibrary = (provider: any): Web3Provider => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 10000;
+  return library;
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
