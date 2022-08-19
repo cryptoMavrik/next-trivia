a90f19bcdb889505f2426b736faee133e373c215
cryptoMavrik <xdaegonx@gmail.com>
2022-08-18 23:10:19 -0400

    patch chain info

--- src/config/chainInfo.ts
+++ src/config/chainInfo.ts
@@ -1,4 +1,5 @@
 export const BASE_EXPLORER_URL = "https://etherscan.io"
-export const COIN_NAME = "ETH"
+export const COIN_NAME = "Ether"
+export const COIN_SYM = "ETH"
 export const CHAIN_NAME = "Ethereum"
 export const RPC_URL = `https://mainnet.infura.io/v3/81cf2554caff4f3da20cf2cf797c578d`
