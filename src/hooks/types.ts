c20335b28879321e09a4ff0bcfcca23c40d7a4bc
cryptoMavrik <xdaegonx@gmail.com>
2022-08-20 10:36:28 -0400

    add strikes for incorrect answers

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,12 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
 
-type Handler = () => void
+type Handler = () => void;
 
 export type GameType = {
     score: number;
     round: number;
+    strikes: number;
     used: number[];
-    questionId: number;
+    questionId?: number;
     selected: number;
     showResults: boolean;
     getNextQuestion: Handler;
@@ -14,4 +15,4 @@ export type GameType = {
     handleNewGame: Handler;
     handleNext: Handler;
     setSelected: Dispatch<SetStateAction<number>>;
-}
+};
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,60 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { questions } from '../config/questions'
-import { GameType } from './types'
+import React, { useState } from "react";
+import { questions } from "../config/questions";
+import { GameType } from "./types";
 
 const useGame = (): GameType => {
-    const [score, setScore] = useState<number>(0)
-    const [round, setRound] = useState<number>(1)
-    const [questionId, setQuestionId] = useState<number>(Math.floor(Math.random() * 100 % 10))
-    const [selected, setSelected] = useState<number>(0)
-    const [showResults, setShowResults] = useState(false)
-    const used = useRef<number[]>([])
+    const [score, setScore] = useState(0);
+    const [round, setRound] = useState(1);
+    const [strikes, setStrikes] = useState(0);
+    const [questionId, setQuestionId] = useState(0);
+    const [selected, setSelected] = useState(0);
+    const [showResults, setShowResults] = useState(false);
+    const [used, setUsed] = useState<number[]>([]);
+    console.log("Strikes", strikes);
 
-    useEffect(() => {
-        console.log("NEW SELECTION", selected);
-
-    }, [selected])
 
     const getNextQuestion = () => {
-        let randomNumber = Math.floor(Math.random() * 100 % 10)
-        if (used.current.includes(randomNumber)) {
-            randomNumber = Math.floor(Math.random() * 100 % 10)
-        }
-        console.log("USED", used.current, randomNumber);
-        setQuestionId(randomNumber > 0 ? randomNumber : 1)
-    }
-
-    const handleNewGame = () => {
-        if (round >= 4) {
-            used.current = []
-            setScore(0);
-            setRound(1);
+        let randomNumber = Math.floor((Math.random() * 100) % 10);
+        if (!used.includes(randomNumber) || questionId !== 0) {
+            console.log("USED", used, randomNumber);
+            setQuestionId(randomNumber > 0 ? randomNumber : 1);
+        } else {
             getNextQuestion();
-            setSelected(0);
-            setShowResults(false);
         }
     };
 
+    const handleNewGame = () => {
+        setStrikes(0);
+        setQuestionId(0);
+        setUsed([]);
+        setScore(0);
+        setRound(1);
+        setSelected(0);
+        handleNext();
+        setShowResults(false);
+    };
+
     const handleSubmitAnswer = () => {
-        if (selected === questions[questionId - 1].correctAnswer) {
-            setScore((prev) => prev + 100 * round);
+        if (questionId && selected === questions[questionId - 1].correctAnswer) {
+            const _score = score + 100 * round
+            setScore(_score);
         } else {
-            setScore((prev) => prev - 100 * round);
+            const _strikes = strikes + 1
+            const _score = score - 100 * round
+            console.log("STRIKES", _strikes);
+
+            setStrikes(_strikes);
+            setScore(_score);
         }
         setShowResults(true);
     };
 
     const handleNext = () => {
-        if (questionId > questions.length) {
-            setShowResults(false);
-            return;
-        } else {
-            used.current.push(questionId)
-            getNextQuestion()
-            setSelected(0);
-            setShowResults(false);
-        }
-        if (used.current.length >= 3 * round && round <= 3) {
+        setQuestionId((prev) => prev + 1);
+        setSelected(0);
+        setShowResults(false);
+        if (questionId >= 3 * round && round <= 3) {
             setRound((prev) => prev + 1);
         }
     };
@@ -62,7 +61,8 @@ const useGame = (): GameType => {
     return {
         score,
         round,
-        used: used.current,
+        strikes,
+        used,
         questionId,
         showResults,
         getNextQuestion,
@@ -70,8 +70,8 @@ const useGame = (): GameType => {
         handleNewGame,
         handleNext,
         setSelected,
-        selected
-    }
-}
+        selected,
+    };
+};
 
-export default useGame
+export default useGame;
