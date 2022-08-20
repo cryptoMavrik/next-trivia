e21d752fb8224abda9c99f0cadd4f6d200efcbdd
cryptoMavrik <xdaegonx@gmail.com>
2022-08-20 10:56:59 -0400

    patch connect component

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useLogin from "../hooks/useLogin";
+import Status from "./Status";
 
 const Connect: React.FC<{ handleNewGame: () => void }> = ({
     handleNewGame,
@@ -8,20 +9,21 @@ const Connect: React.FC<{ handleNewGame: () => void }> = ({
 
     return (
         <div className="container m-auto w-full min-h-screen flex flex-col justify-center items-center">
+            <Status />
             <div className="flex flex-col w-full md:w-2/3 lg:w-3/5 min-h-[30rem] bg-gradient-to-t from-[#090b0e] to-[#10141a] rounded-xl justify-start items-center p-[4rem]">
                 <h1 className="text-3xl lg:text-6xl font-bold text-blue-400">
                     BLOCKCHAIN TRIVIA
                 </h1>
                 <div className="p-5" />
                 <div className="flex flex-col w-full h-full justify-center items-center p-5 text-blue-200">
-                    <span className="p-5 text-2xl ">Connect wallet to play!</span>
+                    <span className="p-3 text-2xl ">Connect wallet to play!</span>
                     <button
-                        className="rounded-md bg-blue-600 w-auto px-[3rem] py-[.5rem] font-bold text-xl hover:bg-blue-600 transition-transform active:translate-y-1"
+                        className="rounded-md bg-blue-700 w-auto px-[3rem] py-[.5rem] font-bold text-xl hover:bg-blue-600 transition-transform active:translate-y-1"
                         onClick={login}
                     >
                         Connect
                     </button>
-                    <button className="pt-5" onClick={() => handleNewGame()}>
+                    <button className="pt-9 hover:text-blue-600" onClick={() => handleNewGame()}>
                         Skip for now
                     </button>
                 </div>
